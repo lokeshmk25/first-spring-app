08581b9981cc5312f014e093bed4b4f731f304cf
lokeshmk25 <lokeshmk2211@gmail.com>
2021-12-02 13:34:55 +0530

    Added: First spring application to demonstrate Rest Api calls

---
+++ src/main/java/com/myfirstapp/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.myfirstapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+    @RequestMapping(value = {"","/","/home"})
+    public String sayHello(){
+        return "Hello world!!";
+    }
+    @RequestMapping(value = {"/query"},method= RequestMethod.GET)
+    public String sayHello(@RequestParam(value="fname")String fname,@RequestParam(value="lname")String lname){
+        return "Hello" +fname+ "" +lname+ "!";
+    }
+}
